7f1b0c4b596c8758a9594dbeefa784d5c8074408
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-01-29 00:11:51 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    int rem;
+    if(num%2==1)
+    {
+        printf("Motu");
+    }
+    else
+    {
+        printf("Patlu");
+    }
+    return 0;
+}
